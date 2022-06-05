f76e6cb4770b0439f598d7d437e0e4b4b89fc525
1-МД-8 Муравейник Татьяна <99969428+mnbvcxzzl@users.noreply.github.com>
2022-06-05 17:09:01 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,7 +1,6 @@
 from langdetect import detect
 
 def get_article_language(article_text):
-    # return detect(article_text)
     try:
         language = detect(article_text)  # Определяем язык статьи
     except TypeError:
@@ -11,10 +10,8 @@ def get_article_language(article_text):
             return ['Русский', ['ru_RU']]
         elif language == 'ru':
             return ['Русский', ['ru_RU']]
+        elif language == 'uk':
+            return ['Русский', ['ru_RU']]
         else:
             return ['Английский', ['en_GB']]
 
-# article_text = 'hello world'
-# article_language = get_article_language(article_text)
-#
-# print(article_language)
