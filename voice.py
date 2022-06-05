3c613d5d6f9c853f6cd2d735ca5cafd0deaf524b
1-МД-8 Муравейник Татьяна <99969428+mnbvcxzzl@users.noreply.github.com>
2022-06-05 17:07:32 +0300

    Update voice.py

--- voice.py
+++ voice.py
@@ -3,24 +3,32 @@ from pydub import AudioSegment
 
 
 def engine_settings(engine, article_language):
-    voices = engine.getProperty('voices')
-    engine.setProperty('rate', 185)  # Выставляем скорость чтения голоса
-    for voice in voices:
-        if voice.languages == article_language and \
-                voice.gender == 'VoiceGenderMale':
-            return engine.setProperty('voice', voice.id)  # Выбираем подходящий голос
+    try:
+        voices = engine.getProperty('voices')
+        engine.setProperty('rate', 185)  # Выставляем скорость чтения голоса
+        for voice in voices:
+            if voice.languages == article_language and \
+                    voice.gender == 'VoiceGenderMale':
+                return engine.setProperty('voice', voice.id)  # Выбираем подходящий голос
+    except:
+        pass
 
 
 def get_mp3_file(file_name, article_text, article_language):
-    engine = pyttsx3.init()
-    engine_settings(engine, article_language)  # Применение настроек голоса
-    engine.save_to_file(article_text, file_name)  # Сохранение текста статьи в аудиофайл
-    engine.runAndWait()
-    convert_file_to_mp3(file_name)  # Конвертация в mp3 формат
+    try:
+        engine = pyttsx3.init()
+        engine_settings(engine, article_language)  # Применение настроек голоса
+        engine.save_to_file(article_text, file_name)  # Сохранение текста статьи в аудиофайл
+        engine.runAndWait()
+        convert_file_to_mp3(file_name)  # Конвертация в mp3 формат
+    except:
+        pass
 
 
 def convert_file_to_mp3(file_name):
-    converter = AudioSegment
-    converter_file = converter.from_file(file_name)
-    converter_file.export(file_name, format="mp3")
-
+    try:
+        converter = AudioSegment
+        converter_file = converter.from_file(file_name)
+        converter_file.export(file_name, format="mp3")
+    except:
+        pass
